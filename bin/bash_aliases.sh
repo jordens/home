019504bcd7d726103cb8f3f5f51ce586b1ec3b30
Robert Jordens <jordens@gmail.com>
2014-08-22 23:13:35 -0600

    lsl grepl

--- bin/bash_aliases.sh
+++ bin/bash_aliases.sh
@@ -6,3 +6,11 @@ alias l='ls -CFG --color=auto'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+function grepl() {
+    grep --color=always "$@" | less -R
+}
+
+function lsl() {
+    ls --color=always "$@" | less -R
+}
