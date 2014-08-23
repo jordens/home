10f9b328f18b774183994d4dc2c9eb61cf27daee
Robert Jordens <jordens@gmail.com>
2014-08-23 16:56:35 -0600

    rename gkring

--- .offlineimap.py
+++ .offlineimap.py
@@ -4,6 +4,6 @@ def get_gmail_pass():
     return get_simple_pass("google_app_password")
 
 def get_simple_pass(key):
-    cmd = "gnome-keyring-get.py simple_name=%s" % key
+    cmd = "gnome-keyring-get simple_name=%s" % key
     for k, v in os.popen(cmd).readline().strip().split(" ", 1):
         return v
