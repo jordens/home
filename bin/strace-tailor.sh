46a322773b227c0eba38bf061785758cec93b87c
Robert Jordens <jordens@gmail.com>
2015-04-27 06:53:58 -0600

    rm strace-tailor: see migen

--- bin/strace-tailor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Copyright Robert Jordens <robert@joerdens.org> 2014,2015
-
-PREFIX=\\/opt\\/Xilinx\\/
-
-strace -e trace=file,process -f -o strace.log $@
-sed -n "s/^.*\"\\(${PREFIX}[^\"]*\\)\".*$/\\1/p" strace.log \
-	| sort | uniq | tr '\n' '\0' \
-	| xargs -0 cp --parent --no-dereference --preserve=all -t .
